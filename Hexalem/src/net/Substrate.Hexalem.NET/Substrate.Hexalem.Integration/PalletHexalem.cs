b075c6cc68d3f94c3e976701ac5d012dd6f9c150
cedric.decoster <cedric.decoster@gmail.com>
2023-11-28 23:15:50 +0100

    added calls

---
+++ Hexalem/src/net/Substrate.Hexalem.NET/Substrate.Hexalem.Integration/PalletHexalem.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json.Linq;
+using Serilog;
+using StrobeNet.Extensions;
+using Substrate.Hexalem.NET.NetApiExt.Generated.Model.pallet_hexalem.pallet;
+using Substrate.Hexalem.NET.NetApiExt.Generated.Model.sp_core.crypto;
+using Substrate.Hexalem.NET.NetApiExt.Generated.Storage;
+using Substrate.Integration.Call;
+using Substrate.Integration.Client;
+using Substrate.Integration.Helper;
+using Substrate.Integration.Model;
+using Substrate.NetApi;
+using Substrate.NetApi.Model.Types;
+using Substrate.NetApi.Model.Types.Base;
+using Substrate.NetApi.Model.Types.Primitive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Substrate.Integration
+{
+    public partial class SubstrateNetwork : BaseClient
+    {
+
+
+        #region storage
+
+
+
+        #endregion storage
+
+        #region call
+
+        /// <summary>
+        /// Create game
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="players"></param>
+        /// <param name="gridSize"></param>
+        /// <param name="concurrentTasks"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> CreateGameAsync(Account account, List<Account> players, byte gridSize, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = $"Hexalem.CreateGame";
+
+            var extrinsic = HexalemModuleCalls.CreateGame(new BaseVec<AccountId32>(players.Select(p => p.ToAccountId32()).ToArray()), new U8(gridSize));
+
+            return await GenericExtrinsicAsync(account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+        /// <summary>
+        /// Play
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="placeIndex"></param>
+        /// <param name="buyIndex"></param>
+        /// <param name="payType"></param>
+        /// <param name="concurrentTasks"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> PlayAsync(Account account, byte placeIndex, byte buyIndex, PayType payType, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = $"Hexalem.Play";
+
+            var enumPayType = new EnumPayType();
+            enumPayType.Create(payType);
+
+            var moveStruct = new Move
+            {
+                PlaceIndex = new U8(placeIndex),
+                BuyIndex = new U8(buyIndex),
+                PayType = enumPayType
+            };
+
+            var extrinsic = HexalemModuleCalls.Play(moveStruct);
+
+            return await GenericExtrinsicAsync(account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+        /// <summary>
+        /// Finish turn
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="concurrentTasks"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<string?> FinishTurnAsync(Account account, int concurrentTasks, CancellationToken token)
+        {
+            var extrinsicType = $"Hexalem.FinishTurn";
+
+            var extrinsic = HexalemModuleCalls.FinishTurn();
+
+            return await GenericExtrinsicAsync(account, extrinsicType, extrinsic, concurrentTasks, token);
+        }
+
+        #endregion call
+    }
+}
