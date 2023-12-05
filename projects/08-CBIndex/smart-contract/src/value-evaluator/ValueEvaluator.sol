5f931dd52ce51aa3957d645de17473215edb857d
zy <cot@zhangyi-desktop>
2023-12-05 04:29:25 +0000

    feat: add evaluator

---
+++ projects/08-CBIndex/smart-contract/src/value-evaluator/ValueEvaluator.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.19;
+
+import "openzeppelin/utils/math/SafeMath.sol";
+import "../utils/VaultFactoryOwnerMixin.sol";
+import "../utils/MathHelpers.sol";
+import "../price-feeds/primitives/ChainlinkPriceFeedMixin.sol";
+import "./IValueEvaluator.sol";
+
+/// @title ValueEvaluator Contract
+/// @notice Interprets price feeds to provide covert value between asset pairs
+contract ValueEvaluator is
+    IValueEvaluator,
+    VaultFactoryOwnerMixin,
+    ChainlinkPriceFeedMixin,
+    MathHelpers
+{
+    using SafeMath for uint256;
+
+    // Used to only tolerate a max rounding discrepancy of 0.01%
+    // when converting values via an inverse rate
+    uint256 private constant _MIN_INVERSE_RATE_AMOUNT = 10000;
+
+    constructor(address _vaultDeployer, address _wethToken, uint256 _chainlinkStaleRateThreshold)
+        VaultFactoryOwnerMixin(_vaultDeployer)
+        ChainlinkPriceFeedMixin(_wethToken, _chainlinkStaleRateThreshold)
+    {}
+
+    // EXTERNAL FUNCTIONS
+
+    /// @notice Calculates the total value of given amounts of assets in a single quote asset
+    /// @param _baseAssets The assets to convert
+    /// @param _amounts The amounts of the _baseAssets to convert
+    /// @param _quoteAsset The asset to which to convert
+    /// @return value_ The sum value of _baseAssets, denominated in the _quoteAsset
+    /// @dev Does not alter protocol state,
+    /// but not a view because calls to price feeds can potentially update third party state.
+    /// Does not handle a derivative quote asset.
+    function calcCanonicalAssetsTotalValue(address[] memory _baseAssets, uint256[] memory _amounts, address _quoteAsset)
+        external
+        override
+        view
+        returns (uint256 value_)
+    {
+        require(_baseAssets.length == _amounts.length, "calcCanonicalAssetsTotalValue: Arrays unequal lengths");
+        require(isSupportedPrimitiveAsset(_quoteAsset), "calcCanonicalAssetsTotalValue: Unsupported _quoteAsset");
+
+        for (uint256 i; i < _baseAssets.length; i++) {
+            uint256 assetValue = __calcAssetValue(_baseAssets[i], _amounts[i], _quoteAsset);
+            value_ = value_.add(assetValue);
+        }
+
+        return value_;
+    }
+
+    // PUBLIC FUNCTIONS
+
+    /// @notice Calculates the value of a given amount of one asset in terms of another asset
+    /// @param _baseAsset The asset from which to convert
+    /// @param _amount The amount of the _baseAsset to convert
+    /// @param _quoteAsset The asset to which to convert
+    /// @return value_ The equivalent quantity in the _quoteAsset
+    /// @dev Does not alter protocol state,
+    /// but not a view because calls to price feeds can potentially update third party state.
+    /// See also __calcPrimitiveToDerivativeValue() for important notes regarding a derivative _quoteAsset.
+    function calcCanonicalAssetValue(address _baseAsset, uint256 _amount, address _quoteAsset)
+        external
+        override
+        view
+        returns (uint256 value_)
+    {
+        if (_baseAsset == _quoteAsset || _amount == 0) {
+            return _amount;
+        }
+
+        if (isSupportedPrimitiveAsset(_quoteAsset)) {
+            return __calcAssetValue(_baseAsset, _amount, _quoteAsset);
+        } 
+        revert("calcCanonicalAssetValue: Unsupported conversion");
+    }
+
+    /// @notice Checks whether an asset is a supported asset
+    /// @param _asset The asset to check
+    /// @return isSupported_ True if the asset is a supported asset
+    function isSupportedAsset(address _asset) public view override returns (bool isSupported_) {
+        return isSupportedPrimitiveAsset(_asset);
+    }
+
+    // PRIVATE FUNCTIONS
+
+    /// @dev Helper to differentially calculate an asset value
+    /// based on if it is a primitive or derivative asset.
+    function __calcAssetValue(address _baseAsset, uint256 _amount, address _quoteAsset)
+        private view
+        returns (uint256 value_)
+    {
+        if (_baseAsset == _quoteAsset || _amount == 0) {
+            return _amount;
+        }
+
+        // Handle case that asset is a primitive
+        if (isSupportedPrimitiveAsset(_baseAsset)) {
+            return __calcCanonicalValue(_baseAsset, _amount, _quoteAsset);
+        }
+
+        revert("__calcAssetValue: Unsupported _baseAsset");
+    }
+
+    ////////////////////////////
+    // PRIMITIVES (CHAINLINK) //
+    ////////////////////////////
+
+    /// @notice Adds a list of primitives with the given aggregator and rateAsset values
+    /// @param _primitives The primitives to add
+    /// @param _aggregators The ordered aggregators corresponding to the list of _primitives
+    /// @param _rateAssets The ordered rate assets corresponding to the list of _primitives
+    function addPrimitives(
+        address[] calldata _primitives,
+        address[] calldata _aggregators,
+        RateAsset[] calldata _rateAssets
+    ) external onlyVaultFactoryOwner {
+        __addPrimitives(_primitives, _aggregators, _rateAssets);
+    }
+
+    /// @notice Removes a list of primitives from the feed
+    /// @param _primitives The primitives to remove
+    function removePrimitives(address[] calldata _primitives) external onlyVaultFactoryOwner {
+        __removePrimitives(_primitives);
+    }
+
+    /// @notice Sets the `ehUsdAggregator` variable value
+    /// @param _nextEthUsdAggregator The `ehUsdAggregator` value to set
+    function setEthUsdAggregator(address _nextEthUsdAggregator) external onlyVaultFactoryOwner {
+        __setEthUsdAggregator(_nextEthUsdAggregator);
+    }
+
+    /// @notice Updates a list of primitives with the given aggregator and rateAsset values
+    /// @param _primitives The primitives to update
+    /// @param _aggregators The ordered aggregators corresponding to the list of _primitives
+    /// @param _rateAssets The ordered rate assets corresponding to the list of _primitives
+    function updatePrimitives(
+        address[] calldata _primitives,
+        address[] calldata _aggregators,
+        RateAsset[] calldata _rateAssets
+    ) external onlyVaultFactoryOwner {
+        __removePrimitives(_primitives);
+        __addPrimitives(_primitives, _aggregators, _rateAssets);
+    }
+
+    // PUBLIC FUNCTIONS
+
+    /// @notice Checks whether an asset is a supported primitive
+    /// @param _asset The asset to check
+    /// @return isSupported_ True if the asset is a supported primitive
+    function isSupportedPrimitiveAsset(address _asset) public view override returns (bool isSupported_) {
+        return _asset == getWethToken() || getAggregatorForPrimitive(_asset) != address(0);
+    }
+}
