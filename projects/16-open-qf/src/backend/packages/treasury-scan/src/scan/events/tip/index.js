50539b00bc88dd678f44fbd6850032953931739d
Yongfeng LI <wliyongfeng@gmail.com>
2023-12-06 21:28:04 +0800

    Add logic to handle tip slashed or retracted

--- projects/16-open-qf/src/backend/packages/treasury-scan/src/scan/events/tip/index.js
+++ projects/16-open-qf/src/backend/packages/treasury-scan/src/scan/events/tip/index.js
@@ -1,5 +1,7 @@
 const { handleNewTip } = require("./newTip");
 const { handleTipClosed } = require("./tipClosed");
+const { handleTipRetracted } = require("./tipRetracted");
+const { handleTipSlashed } = require("./tipSlashed");
 
 async function handleTreasuryTipEvent(event, indexer, extrinsic, blockEvents) {
   const { section, method } = event;
@@ -12,7 +14,9 @@ async function handleTreasuryTipEvent(event, indexer, extrinsic, blockEvents) {
   } else if ("TipClosed" === method) {
     await handleTipClosed(event, indexer, extrinsic, blockEvents);
   } else if ("TipRetracted" === method) {
+    await handleTipRetracted(event);
   } else if ("TipSlashed" === method) {
+    await handleTipSlashed(event);
   }
 }
 
