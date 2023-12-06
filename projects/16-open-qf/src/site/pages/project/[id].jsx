75b7267829209f474aea0b0c615b782775ddeb43
2nthony <ant.hu@outlook.com>
2023-12-06 18:42:31 +0800

    rename to project

--- projects/16-open-qf/src/site/pages/project/[id].jsx
+++ projects/16-open-qf/src/site/pages/project/[id].jsx
@@ -2,9 +2,9 @@ import ListLayout from "@/components/layouts/listLayout";
 import { ROUND_LIST_DATA } from "@/fixtures/roundList";
 import { find } from "lodash-es";
 import { useRouter } from "next/router";
-import ExploreProjectInfo from "@/components/explore/info";
+import ExploreProjectInfo from "@/components/project/info";
 
-export default function ExplorePage() {
+export default function ProjectPage() {
   const router = useRouter();
   const id = Number(router.query.id);
 
