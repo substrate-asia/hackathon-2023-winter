109c00a0b1ad7cafa35d8f5e3b955c80e045254a
2nthony <ant.hu@outlook.com>
2023-12-06 14:59:00 +0800

    feat: home rounds page

--- projects/16-open-qf/src/site/pages/index.jsx
+++ projects/16-open-qf/src/site/pages/index.jsx
@@ -1,12 +1,25 @@
 import MainContainer from "@/components/containers/main";
 import HowOpenQFWorks from "@/components/howOpenQFWorks";
+import { ROUND_LIST_DATA } from "@/fixtures/roundList";
 import AppLayout from "components/layouts/appLayout";
+import RoundCard from "@/components/card/round";
 
 export default function HomePage() {
   return (
     <AppLayout>
       <MainContainer>
-        <div>rounds page</div>
+        <hgroup>
+          <h1 className="text-text-primary text36bold">Program Rounds</h1>
+          <p className="text-text-brand-secondary text16semibold mt-2">
+            How OpenQF Works
+          </p>
+        </hgroup>
+
+        <div className="mt-5 space-y-5">
+          {ROUND_LIST_DATA.map((data, idx) => (
+            <RoundCard key={idx} data={data} />
+          ))}
+        </div>
       </MainContainer>
 
       <HowOpenQFWorks />
