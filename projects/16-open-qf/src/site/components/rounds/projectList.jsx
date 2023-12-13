c238cafa963e3508ed9d3cbe3db4eeb5820d5165
2nthony <ant.hu@outlook.com>
2023-12-13 14:25:50 +0800

    feat: round project list, no data state (#74)
    * nodata
    
    * display no data

--- projects/16-open-qf/src/site/components/rounds/projectList.jsx
+++ projects/16-open-qf/src/site/components/rounds/projectList.jsx
@@ -8,6 +8,7 @@ import Dot from "../dot";
 import IpfsImage from "../image/ipfs";
 import Link from "next/link";
 import NetworkUser from "../user/networkUser";
+import NoData from "../noData";
 
 export default function RoundProjectList({
   categories: categoriesProp = [],
@@ -66,79 +67,85 @@ export default function RoundProjectList({
           <span className="text-text-tertiary">{projects.length}</span>
         </h3>
 
-        <div
-          className={cn(
-            "grid grid-cols-3 gap-5",
-            "max-md:grid-cols-2",
-            "max-sm:grid-cols-1",
-          )}
-        >
-          {filteredProjects.map((project) => (
-            <Card
-              key={project.id}
-              cover={
-                <IpfsImage cid={project.bannerCid} className="object-cover" />
-              }
-              coverPosition="top"
-              size="small"
-              head={
-                <div className="flex flex-col flex-1">
-                  <div className="h-6">
-                    <div
-                      className={cn(
-                        "relative -top-14",
-                        "bg-neutral-200",
-                        "w-16 h-16 rounded-full border-2 border-stroke-border-default",
-                        "overflow-hidden",
-                      )}
-                    >
-                      <IpfsImage cid={project.logoCid} />
-                    </div>
-                  </div>
-                  <div className="flex-1">
-                    <h4 className="text16semibold text-text-primary truncate">
-                      <Link
-                        href={`/rounds/${project.roundId}/projects/${project.id}`}
-                        className="hover:underline hover:text-inherit"
-                      >
-                        {project.name}
-                      </Link>
-                    </h4>
-                    <p className="mt-2 text14medium text-text-secondary line-clamp-3">
-                      {project.summary}
-                    </p>
-                  </div>
+        {filteredProjects.length ? (
+          <ProjectsList projects={filteredProjects} />
+        ) : (
+          <NoData />
+        )}
+      </div>
+    </div>
+  );
+}
 
-                  <div className="mt-5 space-y-1">
-                    <div className="text14medium text-text-tertiary">
-                      Total Raised
-                    </div>
-                    <div className="text16semibold text-text-primary">
-                      123 DOT
-                    </div>
-                    <div className="text12medium text-text-tertiary">
-                      from{" "}
-                      <span className="text-text-primary">
-                        {project.contributors?.length || 0}
-                      </span>{" "}
-                      Contributors
-                    </div>
-                  </div>
-                </div>
-              }
-            >
-              <div className="flex items-center justify-between">
-                <div>
-                  <NetworkUser address={project.creator} network={"polkadot"} />
-                </div>
-                <div>
-                  <Tag size="small">{project.category}</Tag>
+function ProjectsList({ projects = [] }) {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-3 gap-5",
+        "max-md:grid-cols-2",
+        "max-sm:grid-cols-1",
+      )}
+    >
+      {projects.map((project) => (
+        <Card
+          key={project.id}
+          cover={<IpfsImage cid={project.bannerCid} className="object-cover" />}
+          coverPosition="top"
+          size="small"
+          head={
+            <div className="flex flex-col flex-1">
+              <div className="h-6">
+                <div
+                  className={cn(
+                    "relative -top-14",
+                    "bg-neutral-200",
+                    "w-16 h-16 rounded-full border-2 border-stroke-border-default",
+                    "overflow-hidden",
+                  )}
+                >
+                  <IpfsImage cid={project.logoCid} />
                 </div>
               </div>
-            </Card>
-          ))}
-        </div>
-      </div>
+              <div className="flex-1">
+                <h4 className="text16semibold text-text-primary truncate">
+                  <Link
+                    href={`/rounds/${project.roundId}/projects/${project.id}`}
+                    className="hover:underline hover:text-inherit"
+                  >
+                    {project.name}
+                  </Link>
+                </h4>
+                <p className="mt-2 text14medium text-text-secondary line-clamp-3">
+                  {project.summary}
+                </p>
+              </div>
+
+              <div className="mt-5 space-y-1">
+                <div className="text14medium text-text-tertiary">
+                  Total Raised
+                </div>
+                <div className="text16semibold text-text-primary">123 DOT</div>
+                <div className="text12medium text-text-tertiary">
+                  from{" "}
+                  <span className="text-text-primary">
+                    {project.contributors?.length || 0}
+                  </span>{" "}
+                  Contributors
+                </div>
+              </div>
+            </div>
+          }
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <NetworkUser address={project.creator} network={"polkadot"} />
+            </div>
+            <div>
+              <Tag size="small">{project.category}</Tag>
+            </div>
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
