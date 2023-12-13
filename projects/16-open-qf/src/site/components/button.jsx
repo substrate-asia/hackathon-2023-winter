4f8d80c1deafe3e8eb6290737f8e50fe85d376f6
2nthony <ant.hu@outlook.com>
2023-12-13 17:32:50 +0800

    fix: button border

--- projects/16-open-qf/src/site/components/button.jsx
+++ projects/16-open-qf/src/site/components/button.jsx
@@ -2,6 +2,7 @@ import { Button as ButtonBase } from "@osn/common-ui";
 import tw from "tailwind-styled-components";
 
 export const Button = tw(ButtonBase)`
+  !border-fill-bg-brand-secondary
   hover:!border-fill-bg-brand-secondary
   !bg-fill-bg-brand-secondary
   !text-text-primary-contrast
