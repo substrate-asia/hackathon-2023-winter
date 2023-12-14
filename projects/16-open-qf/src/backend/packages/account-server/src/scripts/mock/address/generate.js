2396c92864fabbec2762b6b1633f1d8ce05a1c55
Yongfeng LI <wliyongfeng@gmail.com>
2023-12-14 12:32:37 +0800

    Add a script to log candidate addresses

---
+++ projects/16-open-qf/src/backend/packages/account-server/src/scripts/mock/address/generate.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const {
+  treasury: { getProposalBeneficiaryCol, getBountyBeneficiaryCol, getTipBeneficiaryCol },
+  governance: { getReferendaVoteCol, getReferendaReferendumCol },
+} = require("@open-qf/mongo");
+
+async function queryAddresses(col) {
+  let records = await col.find({}, { projection: { _id: 0, indexer: 0 } }).toArray();
+  return records.map(i => i.address);
+}
+
+(async () => {
+  const proposalBeneficiaries = await queryAddresses(await getProposalBeneficiaryCol());
+  const tipBeneficiaries = await queryAddresses(await getTipBeneficiaryCol());
+  const bountyBeneficiaries = await queryAddresses(await getBountyBeneficiaryCol());
+  const beneficiaries = new Set([
+    ...proposalBeneficiaries,
+    ...tipBeneficiaries,
+    ...bountyBeneficiaries,
+  ]);
+
+  const voterCol = await getReferendaVoteCol();
+  const voters = await voterCol.distinct("account");
+  //todo: other addresses,
+  //  - active voters
+
+  const all = [
+    ...new Set([...beneficiaries, ...voters]),
+  ]
+
+  console.log(all);
+})();
