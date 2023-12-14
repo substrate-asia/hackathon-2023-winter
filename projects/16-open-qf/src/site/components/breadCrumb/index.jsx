b7225b2c31ad82fcbdaaf4a78f5edb2ee12655bf
chaojun <chaojun@opensquare.network>
2023-12-14 14:37:32 +0800

    fix: should have a unique "key" prop.

--- projects/16-open-qf/src/site/components/breadCrumb/index.jsx
+++ projects/16-open-qf/src/site/components/breadCrumb/index.jsx
@@ -8,10 +8,11 @@ export default function BreadCrumb({ items }) {
         <ArrowCaretLeft className="w-[24px] h-[24px]" />
       </div>
       <div className="flex gap-[8px] text16semibold">
-        {items?.map((item) => {
+        {items?.map((item, index) => {
           if (item.url) {
             return (
               <Link
+                key={index}
                 className="cursor-pointer text-text-primary"
                 href={item.url}
               >
