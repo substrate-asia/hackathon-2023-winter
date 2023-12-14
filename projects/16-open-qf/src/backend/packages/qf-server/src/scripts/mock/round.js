5578c05f44ee76f9a965aad0a5d2692157b89790
chaojun <chaojun@opensquare.network>
2023-12-14 10:26:15 +0800

    Add round fields

--- projects/16-open-qf/src/backend/packages/qf-server/src/scripts/mock/round.js
+++ projects/16-open-qf/src/backend/packages/qf-server/src/scripts/mock/round.js
@@ -1,7 +1,8 @@
 const round1 = {
   id: 1,
   title: "Polkadot Infrastructure Funding",
-  description: "The grants focused on the infrastructure projects that pushing Polkadot ecosystem forward. These programs encompass libs, data APIs, bridges, explorers, wallets, governance, and more. Applications submitted by December 15th will be guaranteed a review before the start of the round.",
+  description:
+    "The grants focused on the infrastructure projects that pushing Polkadot ecosystem forward. These programs encompass libs, data APIs, bridges, explorers, wallets, governance, and more. Applications submitted by December 15th will be guaranteed a review before the start of the round.",
   asset: {
     id: "DOT",
     amount: 100000 * Math.pow(10, 10),
@@ -12,8 +13,10 @@ const round1 = {
   },
   founders: ["Polkadot Treasury"],
   bannerCid: "bafybeif2tc7sfljjbg5ftkqderjb3dovawfncucjvga2uiqk6xfhhtcmsu",
+  contributorsCount: 2,
+  category: "Infrastructure",
 };
 
 module.exports = {
   round1,
-}
+};
