e7d1a4f72ef66d620013e4b0fb777d0a79f2c703
xushizhao <49875183@qq.com>
2023-12-16 23:05:28 +0800

    fix 修复不能生成文件的问题

--- projects/24-ThreeCatNew/src/main/java/com/hackathon/framework/utils/TemplateUtil.java
+++ projects/24-ThreeCatNew/src/main/java/com/hackathon/framework/utils/TemplateUtil.java
@@ -28,9 +28,10 @@ public class TemplateUtil {
             //是否为一个文件夹判断
             if(FileUtil.existsFile((String)obj)){
                 //是文件，进行读取文件内容
-                fileStr = FileUtil.readFileToString(((File) obj));
+                fileStr = FileUtil.readFileToString(new File((String)obj));
+
+                System.out.println(fileStr);
                 if (isJsonContent(fileStr)) {
-                    System.out.println("It's JSON content.");
                     //进行内容解析生成测试文件
                     saveTestFile(fileStr,className,saveFilePath);
                 } else {
@@ -47,7 +48,8 @@ public class TemplateUtil {
                 }
             }else if(isJsonContent((String)obj)){
                 //如果是json串格式那么就进行验证生成
-                errText = jsonCheckToClass(fileStr, className, saveFilePath);
+                System.out.println("到这里开始读取");
+                errText = jsonCheckToClass((String)obj, className, saveFilePath);
             }
 
         }  else if (obj instanceof File) {
@@ -69,6 +71,7 @@ public class TemplateUtil {
         if (isJsonContent(fileStr)) {
             //进行内容解析生成测试文件
             saveTestFile(fileStr,className,saveFilePath);
+
         } else {
             errText = "请求数据格式无法解读，请检查数据内容。";
         }
@@ -78,6 +81,8 @@ public class TemplateUtil {
 
     //根据符合格式的json数据，生成对应的测试文件
     public static String saveTestFile(String jsonString,String className,String saveFilePath) throws IOException {
+
+        System.out.println(jsonString);
         String errText = "";
         // 创建 FreeMarker 配置
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_31);
@@ -134,18 +139,16 @@ public class TemplateUtil {
     }
 
     public static void main(String[] args) throws IOException, TemplateException {
-        String jsonString = "[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"y\",\"type\":\"uint256\"}],\"name\":\"add\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"myString\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]";
-
-        String currentProjectPath = System.getProperty("user.dir");
+//        String jsonString = "[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"y\",\"type\":\"uint256\"}],\"name\":\"add\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"myString\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]";
+//        String currentProjectPath = System.getProperty("user.dir");
 //        String testDirectory = currentProjectPath + "/src/test/java/";
-
 //        String o = "e:/longce/";
-
+        String jsonString = "e:/longce/xushizhao.txt";
         String testDirectory = "e:/longce/";
-
         toTestTemplate(jsonString,"MyTestClass",testDirectory);
 
     }
+
 
 
 
