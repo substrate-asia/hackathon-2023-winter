ca5d608e0d388200094cfb2aaad437bef58890ca
hyifeng <hyifeng@gmail.com>
2023-12-18 10:28:36 +0800

    Fix replay mention network (#112)

--- projects/16-open-qf/src/site/components/project/discussion/comment/actions.jsx
+++ projects/16-open-qf/src/site/components/project/discussion/comment/actions.jsx
@@ -2,14 +2,14 @@ import ReplyButton from "@/components/replyButton";
 import { useCallback } from "react";
 import { useEditorContext } from "../editor/context";
 import { fetchIdentity } from "@osn/common";
-import { identityChainMap } from "@osn/constants";
+import { Chains, identityChainMap } from "@osn/constants";
 import { resolveMentionFormat } from "../editor/utils";
 import { useComment } from "./context";
 
 export default function Actions() {
   const comment = useComment();
   const author = comment?.author;
-  const network = comment?.network;
+  const network = Chains.polkadot; // Support polkadot only for now
   const { content, setContent, forceEditor } = useEditorContext();
 
   const onReply = useCallback(() => {
