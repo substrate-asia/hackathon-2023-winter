f4b3e39c3492b17ace0da7ef01c36643650d4dba
hyifeng <hyifeng@gmail.com>
2023-12-18 11:59:35 +0800

    Add matching funds card template (#115)
    * Add matching funds card template
    
    * Update

--- projects/16-open-qf/src/site/components/project/sidebar/index.jsx
+++ projects/16-open-qf/src/site/components/project/sidebar/index.jsx
@@ -1,9 +1,11 @@
 import Contribution from "./contribution";
+import MatchingFunds from "./matchingFunds";
 
 export default function Sidebar() {
   return (
     <div className="flex flex-col gap-[20px]">
       <Contribution />
+      <MatchingFunds />
     </div>
   );
 }
