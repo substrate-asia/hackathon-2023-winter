700799291233ecf2a4f0978683d3a35e4e83e772
hyifeng <hyifeng@gmail.com>
2023-12-19 16:36:52 +0800

    Add node delay test (#148)

--- projects/16-open-qf/src/site/context/nodeDelay/useUpdateNodesDelay.js
+++ projects/16-open-qf/src/site/context/nodeDelay/useUpdateNodesDelay.js
@@ -1,42 +1,7 @@
 import { useAccount } from "@/context/account";
 import { useNodes, useActiveNodeUrl } from "@/context/node";
-import getApi from "@osn/common/es/services/chain/api";
 import { useCallback, useEffect } from "react";
-
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const TIMEOUT = 10000;
-
-const fetchApiTime = async (api) => {
-  const startTime = Date.now();
-  try {
-    await api.rpc.system.chain();
-  } catch (e) {
-    return "error";
-  }
-
-  const endTime = Date.now();
-  return endTime - startTime;
-};
-
-const timeout = async (ms) => {
-  await sleep(ms);
-  return "timeout";
-};
-
-const testNet = async (api) => {
-  return await Promise.race([fetchApiTime(api), timeout(TIMEOUT)]);
-};
-
-const getNodeDelay = async (chain, url) => {
-  try {
-    const api = await getApi(chain, url);
-    return await testNet(api);
-  } catch (e) {
-    console.error("we have a error to test network", e);
-    return "timeout";
-  }
-};
+import { getNodeDelay } from "./getNodeDelay";
 
 export const useUpdateNodesDelay = (setNodesDelay) => {
   const nodes = useNodes();
