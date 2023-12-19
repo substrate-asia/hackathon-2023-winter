d2660d374b2f5993a1b080e715a552c65db2be2c
2nthony <ant.hu@outlook.com>
2023-12-19 11:39:23 +0800

    test: locale symbol (#141)
    * update
    
    * test: locale symbol

--- projects/16-open-qf/src/site/vitest.config.js
+++ projects/16-open-qf/src/site/vitest.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from "vitest/config";
+import path from "node:path";
 
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
+});
