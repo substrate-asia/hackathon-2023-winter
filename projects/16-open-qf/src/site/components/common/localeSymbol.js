fd8423f1c13bc4f7b0105e5c9eed928dcb6f9db9
2nthony <ant.hu@outlook.com>
2023-12-19 11:19:50 +0800

    fix: no value decimals

--- projects/16-open-qf/src/site/components/common/localeSymbol.js
+++ projects/16-open-qf/src/site/components/common/localeSymbol.js
@@ -4,6 +4,7 @@ export default function LocaleSymbol({ value }) {
   const decimals = 10;
   return `${new BigNumber(value)
     .div(Math.pow(10, decimals))
+    .decimalPlaces(0)
     .toNumber()
     .toLocaleString()} DOT`;
 }
