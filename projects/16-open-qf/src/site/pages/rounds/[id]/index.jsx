47cd3e00e3549c6ad58d86baf6d600a49b4c7070
2nthony <ant.hu@outlook.com>
2023-12-20 11:17:29 +0800

    fix: round detail backdrop (#155)

--- projects/16-open-qf/src/site/pages/rounds/[id]/index.jsx
+++ projects/16-open-qf/src/site/pages/rounds/[id]/index.jsx
@@ -8,10 +8,30 @@ import {
   getRoundProjectsList,
   getRoundsList,
 } from "@/services/rounds";
+import { cn } from "@/utils";
 
 const RoundPage = withCommonPageWrapper(({ round, projects, categories }) => {
   return (
-    <ListLayout title="Explore Projects" description="How OpenQF Works">
+    <ListLayout
+      title="Explore Projects"
+      description="How OpenQF Works"
+      backdrop={
+        <div className="w-full h-full relative">
+          <div
+            className={cn(
+              "absolute inset-0",
+              "w-inherit h-inherit",
+              "bg-gradient-to-b from-transparent from-[-100%] to-white to-100%",
+            )}
+          />
+          <img
+            src="/imgs/imgs-round1-bg.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      }
+    >
       <div className="space-y-5">
         <RoundProjectInfo data={round} />
 
