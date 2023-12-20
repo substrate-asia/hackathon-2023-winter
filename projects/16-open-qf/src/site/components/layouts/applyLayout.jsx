900de63adf1f60272abd0f1ea716d5bb5f593775
hyifeng <hyifeng@gmail.com>
2023-12-20 18:02:24 +0800

    fix: apply page header bg (#163)
    * fix: apply page header bg
    
    * fix: use backdrop

--- projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
+++ projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
@@ -2,12 +2,14 @@ import { Breadcrumb } from "@osn/common-ui";
 import MainContainer from "../containers/main";
 import AppLayout from "./appLayout";
 import { useRouter } from "next/router";
+import Backdrop from "./backdrop";
 
 export default function ApplyLayout({ children }) {
   const router = useRouter();
 
   return (
     <AppLayout>
+      <Backdrop className="h-[308px]" />
       <MainContainer className="pt-10 space-y-5">
         <Breadcrumb
           backButtonRender={(button) => (
--- projects/16-open-qf/src/site/components/layouts/detailLayout.jsx
+++ projects/16-open-qf/src/site/components/layouts/detailLayout.jsx
@@ -1,5 +1,6 @@
 import MainContainer from "../containers/main";
 import AppLayout from "./appLayout";
+import Backdrop from "./backdrop";
 
 export default function DetailLayout({
   children,
@@ -9,16 +10,14 @@ export default function DetailLayout({
 }) {
   return (
     <AppLayout {...props}>
-      <div className="flex flex-col relative">
-        <div className="absolute z-0 left-0 right-0 top-0 h-[240px] bg-fill-bg-primary border-b border-stroke-border-default"></div>
-        <MainContainer className="z-10 pt-10">
-          <div className="mb-[20px]">{breadcrumb}</div>
-          <div className="flex gap-[20px]">
-            <div className="flex grow">{children}</div>
-            <div className="max-md:hidden min-w-[392px]">{sidebar}</div>
-          </div>
-        </MainContainer>
-      </div>
+      <Backdrop className="h-[240px]" />
+      <MainContainer className="pt-10">
+        <div className="mb-[20px]">{breadcrumb}</div>
+        <div className="flex gap-[20px]">
+          <div className="flex grow">{children}</div>
+          <div className="max-md:hidden min-w-[392px]">{sidebar}</div>
+        </div>
+      </MainContainer>
     </AppLayout>
   );
 }
