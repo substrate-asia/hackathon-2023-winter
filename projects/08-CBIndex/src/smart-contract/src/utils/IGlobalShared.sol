54a436e525d2fdb2bd96a0f322dfa66f512f2df4
zy <cot@zhangyi-desktop>
2023-12-21 06:53:33 +0000

    update: clean smart contract

--- projects/08-CBIndex/src/smart-contract/src/utils/IGlobalShared.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.19;
-
-interface IGlobalShared {
-    function getVaultFactory() external view returns (address);
-    function getIntegrationManager() external view returns (address);
-    function getValueEvaluator() external view returns (address);
-    function getWethToken() external view returns (address);
-}
