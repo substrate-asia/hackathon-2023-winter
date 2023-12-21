a22ac445859a9eb117c7cfcdf4b649a343e48201
hyifeng <hyifeng@gmail.com>
2023-12-21 10:34:19 +0800

    Refactor github connect logic to backend (#166)
    * Refactor github connect logic to backend
    
    * Update

--- projects/16-open-qf/src/backend/packages/qf-server/src/features/github/controllers/createGithubUser.js
+++ projects/16-open-qf/src/backend/packages/qf-server/src/features/github/controllers/createGithubUser.js
@@ -5,8 +5,47 @@ const { getGithubUserCol } = require("@open-qf/mongo/src/qf");
 const { HttpError } = require("../../../utils/httpError");
 const { checkSignature } = require("../../../utils/checkSignature");
 
+async function fetchGithubUser(code) {
+  const resp = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_OAUTH_CLIENT_ID,
+      client_secret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
+      code,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!resp.ok) {
+    throw new HttpError(500, "Failed to fetch access token");
+  }
+
+  const data = await resp.json();
+
+  const { access_token, token_type } = data;
+
+  if (token_type !== "bearer") {
+    throw new HttpError(500, "Invalid token type");
+  }
+
+  const userResp = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${access_token}`,
+    },
+  });
+
+  if (!userResp.ok) {
+    throw new HttpError(500, "Failed to fetch user info");
+  }
+
+  return await userResp.json();
+}
+
 async function createGithubUser(ctx) {
-  const { address, signature, user } = ctx.request.body;
+  const { address, signature, code } = ctx.request.body;
 
   const col = await getGithubUserCol();
   const dbUser = await col.findOne({ address });
@@ -19,7 +58,10 @@ async function createGithubUser(ctx) {
   }
 
   await checkSignature(challenge, signature, address);
+
+  const user = await fetchGithubUser(code);
   await upsertGithubUser({ address, signature, user });
+
   ctx.body = {
     success: true,
   };
--- projects/16-open-qf/src/site/pages/oauth/callback/github/index.jsx
+++ projects/16-open-qf/src/site/pages/oauth/callback/github/index.jsx
@@ -13,61 +13,15 @@ export const getServerSideProps = async (context) => {
     return to404();
   }
 
-  const resp = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    body: JSON.stringify({
-      client_id: process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID,
-      client_secret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
-      redirect_uri: process.env.NEXT_PUBLIC_GITHUB_OAUTH_REDIRECT_URI,
-      code,
-    }),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (resp.status === 404) {
-    return to404();
-  }
-
-  if (!resp.ok) {
-    throw new Error("Failed to fetch access token");
-  }
-
-  const data = await resp.json();
-
-  const { access_token, token_type } = data;
-
-  if (token_type !== "bearer") {
-    throw new Error("Invalid token type");
-  }
-
-  const userResp = await fetch("https://api.github.com/user", {
-    headers: {
-      Authorization: `token ${access_token}`,
-    },
-  });
-
-  if (userResp.status === 404) {
-    return to404();
-  }
-
-  if (!userResp.ok) {
-    throw new Error("Failed to fetch user");
-  }
-
-  const user = await userResp.json();
-
   const [address, signature] = state.split(",");
   const { result } = await ssrNextApi.post("/github/users", {
     address,
     signature,
-    user,
+    code,
   });
 
   if (!result) {
-    throw new Error("Failed to save github user info");
+    return to404();
   }
 
   return redirect(`/users/${address}`);
